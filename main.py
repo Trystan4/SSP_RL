6861a9fb27c0740edd5dfc5add5ce0786c9e04ac
trystan.roches <trystan.roches@dataimmosolutions.com>
2022-05-06 13:06:39 +0200

    refactor: user choices

--- main.py
+++ main.py
@@ -2,59 +2,63 @@
 # -*- coding: utf-8 -*- 
 
 import matplotlib.pyplot as plt
+import sys
 from benchmark.algorithms.q_learning import grid_qlearning
 from benchmark.algorithms.sarsa import grid_sarsa_v1
 
 NOT_IMPLEMENTED = "pas encore implémenté"
+environnement = {
+    0 : "GridWorld-v0",
+    1 : "FrozenLake-v1",
+    2 : "FrozenLake8x8-v1"
+}
 
-
-if __name__ == "__main__":
-    print("Quel environnement? (GridWorld-v0, FrozenLake-v1, FrozenLake8x8-v1) :")
-    environnement = input()
-    print("Quel(s) algorithmes voulez vous lancer? (Qlearning, SARSA, DQN, Reinforce)")
-    algo = input()
-    if(algo.lower() == "qlearning") :
-        print("Variable bêta :")
-        beta = float(input())  # often 0.5
-        print("Variable epsilon :")
-        epsilon = float(input()) # often 0.5
-        print("Combien de fois voulez vous lancer l'environnement?")
-        max_episodes = int(input())
+def main():
+    print("Quel environnement voulez-vous?")
+    choice_env = int(input("0: GridWorld-v0 \n1: FrozenLake-v1 \n2: FrozenLake8x8-v1\n"))
+    
+    print("Quel(s) algorithmes voulez vous lancer?")
+    algo = input("0: Qlearning \n1: Sarsa \n2: DQN \n3: Reinforce\n")
+    
+    if(int(algo) == 0) :
+        beta = float(input("Variable bêta :\n"))  # often 0.5
+        epsilon = float(input("Variable epsilon :\n")) # often 0.5
+        max_episodes = int(input("Combien de fois voulez vous lancer l'environnement?\n"))
         
-        qln_algo = grid_qlearning.Qlearning(environnement, beta, epsilon, max_episodes)
+        qln_algo = grid_qlearning.Qlearning(environnement[choice_env], beta, epsilon, max_episodes)
         q_table, pi, rewards = qln_algo.algorithm()
-        print("Q table Q Learning")
-        print(q_table)
-        print("politique=",pi)
         
-        plt.plot(rewards)
-        plt.show()
+        print("Q table Q Learning :\n", q_table)
+        print("politique = ",pi)
+        # print("Gains :\n", rewards[:100])
+        # plt.plot(rewards)
+        # plt.show()
         
-    elif(algo.lower() == "sarsa"):
+    elif(int(algo) == 1):
+        total_episodes = int(input("Combien de fois voulez vous lancer l'environnement?\n"))
+        max_steps = int(input("Maximum d'actions par épisode : (max 100)\n"))
+        epsilon = float(input("Variable epsilon :\n")) # often 0.9
+        alpha = float(input("Variable alpha :\n")) # often 0.85
+        gamma = float(input("Variable gamma :\n")) # often 0.95
         
-        print("Combien de fois voulez vous lancer l'environnement?")
-        total_episodes = int(input())
-        print("Maximum d'actions par épisode : (max 100)")
-        max_steps = int(input())
-        print("Variable epsilon :")
-        epsilon = float(input()) # often 0.9
-        print("Variable alpha :")
-        alpha = float(input()) # often 0.85
-        print("Variable gamma :")
-        gamma = float(input()) # often 0.95
-        
-        sarsa_algo = grid_sarsa_v1.sarsa(environnement, epsilon, total_episodes, max_steps, alpha, gamma )
+        sarsa_algo = grid_sarsa_v1.sarsa(environnement[choice_env], epsilon, total_episodes, max_steps, alpha, gamma )
         q_table, performance, rewards = sarsa_algo.algorithm()
+        
         #Visualizing the Q-matrix
         print("Q table Sarsa : \n",q_table)
         print("Performance :", performance)
-        plt.plot(rewards[:100])
-        plt.show()
+        # print("Gains :\n", rewards[:100])
+        # plt.plot(rewards)
+        # plt.show()
         
-    elif(algo.lower() == "dqn"):
+    elif(int(algo) == 2):
         print(NOT_IMPLEMENTED)
-    elif(algo.lower() == "reinforce"):
+    elif(int(algo) == 3):
         print(NOT_IMPLEMENTED)
     else:
         print("algorithme non valable")
-    
+        
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())    
